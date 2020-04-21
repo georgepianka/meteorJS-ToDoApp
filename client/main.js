f8c6ca9f4aefb95c2ba0b187bf71cedaa618d367
NotoriousCottonball <gpianka1@gmail.com>
2020-04-20 19:32:54 -0700

    Create todos collection and display in main template

--- client/main.js
+++ client/main.js
@@ -4,10 +4,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-
+import './main.css';
 import './main.html';
+import { Todos } from '../todos.js';
 
-Template.hello.onCreated(function helloOnCreated() {
+
+Template.main.helpers({
+  todos() {
+      return Todos.find();
+  }
+});
+
+//window.Todos = Todos; // here we make it global
+
+/*And then as long as this file is imported by client/main.js, you can use Todos on the console.
+
+This is fine for small apps and testing stuff, but increases the likelihood of name collisions and makes it harder to understand where code is coming from in larger apps.
+
+If you want to do things the "right" way for a module system, you can access the exports of a module in the console with require:
+
+> Todos = require('/todos').Todos
+
+*/
+
+/*Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
 });
@@ -24,3 +44,5 @@ Template.hello.events({
     instance.counter.set(instance.counter.get() + 1);
   },
 });
+
+*/
--- server/main.js
+++ server/main.js
@@ -1,5 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-
+import { Todos } from '../todos.js';
 Meteor.startup(() => {
   // code to run on server at startup
 });
